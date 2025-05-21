09adce1206c83c8eb976a04010536106b3094e05
supriyaa1812 <supriya.n@s.amity.edu>
2025-05-21 14:53:34 +0530

    Added POST method for creating users

--- app.py
+++ app.py
@@ -1,5 +1,13 @@
 # --- User Management ---
-@app.route('/users', methods=['GET'])
+@app.route('/users', methods=['GET', 'POST'])
 def manage_users():
     if request.method == 'GET':
         return jsonify(list(users.values()))
+    elif request.method == 'POST':
+        data = request.json
+        name = data.get('name', '').strip()
+        if not name:
+            return jsonify({'error':'Name required'}), 400
+        user_id = str(len(users) + 1)
+        users[user_id] = {'id': user_id, 'name': name}
+        return jsonify(users[user_id]), 201
